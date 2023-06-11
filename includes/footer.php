5df487c0d7b37f13c7573321f33cfc7b966c1689
SUHAS B M <91217762+i-suhas@users.noreply.github.com>
2023-06-12 00:55:26 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+</div>
+
+<style>
+body {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+footer {
+  margin-top: auto;
+}
+
+</style>
+<footer> copyright@2023 .All rights reserved
+               </footer>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require "includes/header.php"; ?>
+ <h2>welcome to my index page </h2>
+<?php require "includes/footer.php"; ?>
